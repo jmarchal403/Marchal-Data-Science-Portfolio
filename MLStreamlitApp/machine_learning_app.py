007a332ba58fd0b8bc054dc6744383ff2ea369c3
jmarchal403 <jmarchal@nd.edu>
2026-05-06 21:19:10 -0400

    Update machine_learning_app.py

--- MLStreamlitApp/machine_learning_app.py
+++ MLStreamlitApp/machine_learning_app.py
@@ -14,7 +14,7 @@ st.divider()
 
 
 st.header("What is Supervised Machine Learning?")
-st.write("Supervised machine learning is a type of machine learning where the model is trained on a labeled dataset, meaning that the input data is paired with the correct output labels. The goal is to learn a mapping from inputs to outputs so that the model can make predictions on new, unseen data.")
+st.write("Supervised machine learning is a type of machine learning where the model is trained on a labeled dataset, meaning that the input data is paired with the correct output labels. The goal is for the model to learn the association between certain features and correct labels in order to make accurate predictions on new, unseen data.")
 
 st.subheader("What does this app do specifically?")
 st.write("This app will allow you to explore a dataset of your own or choose a sample dataset provided to explore three common supervised machine learning models:")
@@ -70,6 +70,7 @@ if len(df_encoded) > 500:
 st.subheader("Don't forget to clean!")
 st.write("The dataset has been cleaned by dropping rows with missing values and converting categorical variables into dummy variables. Here's a preview of the cleaned dataset:")
 st.dataframe(df_encoded.head())
+colored_box("Feedback: Cleaning is an important step in supervised machine learning because many models cannot handle missing values or raw text", color ="#34d16b")
     
 st.divider()
 
@@ -131,8 +132,11 @@ if technique_options == "Linear Regression":
         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
         st.write(f"R squared: {r2}")
         st.write(f"Root Mean Squared Error: {rmse}")
-        colored_box("Feedback: R squared is a measure of how well the model explains the variance in the target variable. A higher R squared value indicates a better fit of the model to the data. RMSE is a measure of the average distance between the predicted values and the actual values. A lower RMSE value indicates better performance of the model.", color="#34d16b")
-
+        colored_box("Feedback: R squared is a measure of how well the model explains the variance in the target variable. A higher R squared value (closer to 1 than 0) indicates a better fit of the model to the data. RMSE is a measure of the average distance between the predicted values and the actual values. A lower RMSE value indicates better performance of the model. The coefficients tell us the impact of one additional unit increase from each feature variable on our target variable. The intercept is the value of our target variavble when all selected feature variables are at zero", color="#34d16b")
+    st.divider()
+    st.subheader("Conclusion")
+    st.write("That concludes the linear regression section of this app. However, I encourage you to keep experimenting with different feature and target variables to discover any surprising relationships and reduce the RMSE. You are also welcome to toggle to the other supervised machine learning methods in this app.")
+    st.write("If you would like to learn more about linear regression in machine learning, please feel free to visit this page from GeeksForGeeks: https://www.geeksforgeeks.org/machine-learning/ml-linear-regression/")
 
 
 
@@ -152,13 +156,13 @@ elif technique_options == "Decision Tree":
     from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, roc_curve, roc_auc_score, accuracy_score, precision_score
     st.write("Great! You selected decision tree. A decision tree is a type of supervised machine learning model that uses a tree-like structure to make decisions based on the feature variables. The model splits the data into branches based on the values of the feature variables, and each branch represents a possible outcome or decision.")
     with st.expander("How it works (in more detail):"):
-            colored_box("The decision tree algorithm works by recursively splitting the data into subsets based on the feature variables. The algorithm selects the feature variable and the corresponding threshold that best separates the data into different classes (for classification) or minimizes the variance (for regression). This process continues until a stopping criterion is met, such as reaching a maximum depth or having a minimum number of samples in a leaf node. The resulting tree structure can be used to make predictions by traversing the tree from the root to a leaf node based on the values of the feature variables.", color="#dac17f")    
+            colored_box("The decision tree algorithm works by splitting the data into subsets based on the feature variables. The algorithm selects the feature variable and the corresponding threshold that best separates the data into different classes (for classification) or minimizes the variance (for regression). This process continues until a stopping criterion is met, such as reaching a maximum depth or having a minimum number of samples in a leaf node. The resulting tree structure can be used to make predictions by traversing the tree from the root to a leaf node based on the values of the feature variables.", color="#dac17f")    
     # Now, we should allow the user to select a target variable and feature variables of their own for the decision tree analysis. However, because the sample datasets work mostly with numerical variables, we need to transform our numerical target variable into a categorical variable for the decision tree model. We will create new categorical target variables for each dataset to use in the decision tree section of the app.
     st.write("For the decision tree section of this app, we will be working with the same datasets as before, but we will need to transform the numerical target variables into categorical variables for the deicion tree model we'll build:")
-    colored_box("For the Flight Data, we will create a new target variable called 'Delayed' which will indicate whether a flight was delayed (ARR_DELAY > 0) or not (ARR_DELAY <= 0). ", color="#679edc")
-    colored_box("For the Weather Data, we will create a new target variable called 'Rain' which will indicate whether there was rainfall (Rainfall > 0) or not (Rainfall <= 0).)", color="#679edc")
-    colored_box("For the Motor Trends Data, we will create a new target variable called 'Fuel_Efficient' which will indicate whether a car is fuel efficient (mpg > 30) or not (mpg <= 30).", color="#679edc")
-    colored_box("If you have uploaded your own dataset, your options will be limited to the categorical variables available in your dataset.", color="#679edc")
+    colored_box("For the Flight Data, we will create a new target variable called 'Delayed' which will indicate whether a flight was delayed (ARR_DELAY > 0) or not (ARR_DELAY <= 0). ", color="#a3c1e3")
+    colored_box("For the Weather Data, we will create a new target variable called 'Rain' which will indicate whether there was rainfall (Rainfall > 0) or not (Rainfall <= 0).)", color="#a3c1e3")
+    colored_box("For the Motor Trends Data, we will create a new target variable called 'Fuel_Efficient' which will indicate whether a car is fuel efficient (mpg > 30) or not (mpg <= 30).", color="#a3c1e3")
+    colored_box("If you have uploaded your own dataset, your options will be limited to the categorical variables available in your dataset.", color="#a3c1e3")
     if dataset_option == "Flight Data" and "ARR_DELAY" in df_encoded.columns:
         df_encoded["Delayed"] = df_encoded["ARR_DELAY"].apply(lambda x: 1 if x > 0 else 0)
     elif dataset_option == "Weather Data" and "Rainfall" in df_encoded.columns:
@@ -192,9 +196,10 @@ elif technique_options == "Decision Tree":
 
     
     max_depth = st.slider("Select the max depth for the decision tree", min_value=1, max_value=15, value=4)
-    colored_box("Guide: The max depth of the decision tree is a hyperparameters that decides how many splits the tree will make before it stops. A lower max depth will have fewer splits but may underfit the data. A higher max depth will have more splits but may end up overfitting the data. You should experiment with several different values to see how it impacts the model performance.", color="#f0a9ba")
+    colored_box("Guide: The max depth of the decision tree is a hyperparameter that decides how many splits the tree will make before it stops. A lower max depth will have fewer splits but may underfit the data. A higher max depth will have more splits but may end up overfitting the data. You should experiment with several different values to see how it impacts the model performance.", color="#e7ea9e")
     min_samples_split = st.slider("Select the minimum samples split for the decision tree", min_value=2, max_value=20, value=2)
-    colored_box("Guide: The minimum samples split is another decision tree hyperparameter that determines when the model is allowed to split a node. It sets a minimum requirement of data points in a node before it can even be split. Lowering the minimum may lead to overfitting, while setting the minimum too high may underfit the data. Again, we recommend experimenting with this hyperparameter to see how it impacts performance", color = "#f0a9ba")
+    colored_box("Guide: The minimum samples split is another decision tree hyperparameter that determines when the model is allowed to split a node. It sets a minimum requirement of data points in a node before it can even be split. Lowering the minimum may lead to overfitting, while setting the minimum too high may underfit the data. Again, we recommend experimenting with this hyperparameter to see how it impacts performance", color = "#e7ea9e")
+    st.write()
     if target_variable in feature_variables:
         st.error("You cannot select the same column as both a target variable and a feature variable. Please re-select.")
     elif not target_variable or not feature_variables:
@@ -218,23 +223,17 @@ elif technique_options == "Decision Tree":
             st.write(f"Accuracy: {accuracy:.2f}")
             st.write(f"Precision: {precision:.2f}")
             # Now provide feedback/guide on what accuracy and precision scores mean
-            colored_box("Guide: Accuracy scores record the portion of all predictions that our model correctly predicted. Precision scores tell us the portion of positives our model correctly predicted.", color = "#f0a9ba")
+            colored_box("Guide: Accuracy scores record the portion of all predictions that our model correctly predicted. Precision scores tell us the portion of positives our model correctly predicted.", color = "#e7ea9e")
             if y.unique().shape[0] == 2:
                 y_prob = model.predict_proba(X_test)[:, 1]
                 roc_auc = roc_auc_score(y_test, y_prob)
-                
-            #else: 
-                #st.write("ROC AUC Score is not applicable for multi-class classification. It is only applied to binary target variables.")
-
-            #st.write("Classification Report:")
-            #st.write(classification_report(y_test, y_pred))
             st.subheader("Confusion Matrix:")
             cm = confusion_matrix(y_test, y_pred)
             disp = ConfusionMatrixDisplay(confusion_matrix=cm)
             fig_cm, ax_cm = plt.subplots()
             disp.plot(ax=ax_cm, cmap=plt.cm.Blues)
             st.pyplot(fig_cm)
-            colored_box("Guide: The Confusion Matrix gives us a visualization of where the model is predicting correctly, and where it is predicting incorrectly. In the top left, we can see how many negatives were correctly identified. In the bottom right, we can see how many positives were correctly identified", color = "#f0a9ba")
+            colored_box("Guide: The Confusion Matrix gives us a visualization of where the model is predicting correctly, and where it is predicting incorrectly. In the top left, we can see how many negatives were correctly identified. In the bottom right, we can see how many positives were correctly identified", color = "#e7ea9e")
             plt.clf()  # Clear the figure to avoid overlap with next plot
             if y.nunique() == 2:
                 st.subheader("ROC Curve:")
@@ -248,7 +247,7 @@ elif technique_options == "Decision Tree":
                 ax_roc.legend()
                 st.pyplot(fig_roc)
                 st.write(f"ROC AUC Score: {roc_auc:.2f}")
-                colored_box("Guide: The ROC Curve displays the tradeoff between true positives and true negatives. When the curve is farther away from the straight line, it is associated with better model performance.", color = "#f0a9ba")
+                colored_box("Guide: The ROC Curve presents the tradeoff between true positives and true negatives. When the curve is farther away from the straight line and curves further into the top left portion of the graph, it is associated with better model performance. The ROC AUC score condenses this performance into one number. Scores closer to 1 indicate that the model is doing well at correctly classifying datapoints. A score around 0.5 means that the model is doing about as well as it would if it guessed randomly.", color = "#e7ea9e")
         
                 plt.clf()  # Clear the figure to avoid overlap with next plot
             st.subheader("Decision Tree Visualization:")
@@ -256,9 +255,12 @@ elif technique_options == "Decision Tree":
             plot_tree(model, feature_names=feature_variables, class_names=[str(cls) for cls in model.classes_], filled=True, ax=ax_tree)
             st.pyplot(fig_tree)
             # Now provide feedback on the 
-            colored_box("Guide: The decision tree visualization tells us which feature variables were used to split the data.", color = "#f0a9ba")
+            colored_box("Guide: The decision tree visualization tells us which feature variables were used to split the data. It functions as a visual of the logical decision making process that the model goes through when classifying the data. The splits at the top of the tree are typically the most important while splits further down the tree are usually smaller and more specific.", color = "#e7ea9e")
             plt.clf()  # Clear the figure to avoid overlap with next plot
-
+    st.divider()
+    st.subheader("Conclusion")
+    st.write("That concludes the Decision Tree section of this app. However, I encourage you to keep experimenting with different variables, max depths, and minimum samples split. You are also welcome to toggle to the other supervised machine learning methods if you haven't done so.")
+    st.write("If you would like to learn more about decision trees, feel free to check out this page from the DEV Community site: https://dev.to/arbashhussain/a-step-by-step-guide-to-decision-trees-in-machine-learning-3h8h")
 
 
     
@@ -352,7 +354,9 @@ elif technique_options == "K-Nearest Neighbors (KNN)":
         st.write(f"Precision: {precision:.2f}")
         if y.nunique() == 2:
             st.write(f"ROC AUC Score: {roc_auc:.2f}")
+            colored_box("Guide: Accuracy scores record the portion of all predictions that our model correctly predicted. Precision scores tell us the portion of positives our model correctly predicted. The ROC AUC score measures how well the model classifies positive instances compared to negative instances. A score at 0.5 is equivalent to random guessing. The closer the score is to 1, the better the performance of the model.", color = "#e7ea9e")
         else:
+            colored_box("Guide: Accuracy scores record the portion of all predictions that our model correctly predicted. Precision scores tell us the portion of positives our model correctly predicted.", color = "#e7ea9e")
             st.write("ROC AUC Score is only shown for binary classification problems")
         # Display the classification report for the KNN model
         st.subheader("Confusion Matrix")
@@ -361,6 +365,8 @@ elif technique_options == "K-Nearest Neighbors (KNN)":
         fig_cm, ax_cm = plt.subplots()
         ConfusionMatrixDisplay(confusion_matrix=cm).plot(ax=ax_cm)
         st.pyplot(fig_cm)
+        colored_box("Guide: The Confusion Matrix gives us a visualization of where the model is predicting correctly, and where it is predicting incorrectly. In the top left, we can see how many negatives were correctly identified. In the bottom right, we can see how many positives were correctly identified", color = "#e7ea9e")
+
         plt.clf()  # Clear the figure to avoid overlap with next plot
         # Display the ROC curve for the KNN model
         if y.nunique() ==2:
@@ -374,7 +380,13 @@ elif technique_options == "K-Nearest Neighbors (KNN)":
             ax_roc.set_title('ROC Curve')
             ax_roc.legend()
             st.pyplot(fig_roc)
+            colored_box("Guide: The ROC Curve presents the tradeoff between true positives and true negatives. When the curve is farther away from the straight line and curves further into the top left portion of the graph, it is associated with better model performance.", color = "#e7ea9e")
+        
             plt.clf()  # Clear the figure to avoid overlap with next plot
+    st.divider()
+    st.subheader("Conclusion")
+    st.write("That concludes the K-Nearest Neighbors section of this app. However, I encourage you to keep experimenting with different variables, number of neighbors, and weighting methods. You can also toggle to different supervised machine learning methods in this app if you haven't done so.")
+    st.write("If you would like to learn more about K-Nearest Neighbors, feel free to explore this page from GeeksForGeeks: https://www.geeksforgeeks.org/machine-learning/k-nearest-neighbours/")
 
 
 
